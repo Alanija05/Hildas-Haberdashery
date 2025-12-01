76b334ae4ce5854a240edfefda07369755c24d52
Alan Walendzik <alan.avat@gmail.com>
2025-12-01 10:29:13 +0000

    Sprint 3

--- hildas-haberdashery.py
+++ hildas-haberdashery.py
@@ -1,24 +1,65 @@
 # All products are listed below
 stock_list = [
-    ["Sewing thread", 1.20, 184],
-    ["Zipper", 0.65, 97],
-    ["Wooden buttons", 1.80, 142],
-    ["Iron on interfacing", 2.50, 76],
-    ["Bias binding", 1.10, 213],
-    ["Hook-and-eye set", 0.90, 58],
-    ["Seam ripper", 1.50, 34],
-    ["Tailors chalk", 1.25, 89],
-    ["Elastic", 0.75, 167],
-    ["Thimble", 1, 121]
+    ["sewing thread", 1.20, 184],
+    ["zipper", 0.65, 97],
+    ["wooden buttons", 1.80, 142],
+    ["iron on interfacing", 2.50, 76],
+    ["bias binding", 1.10, 213],
+    ["hook-and-eye set", 0.90, 58],
+    ["seam ripper", 1.50, 34],
+    ["tailors chalk", 1.25, 89],
+    ["elastic", 0.75, 167],
+    ["thimble", 1, 121]
 ]
 
-# Total stock price calculation
-total_stock_price = 0
 
-for item in stock_list:
-    item_stock_price = item[1] * item[2]
-    total_stock_price += item_stock_price
+exit_system = False
 
+print("Welcome to Hilda's Haberdashery system! What would you like to do:")
+while exit_system == False:
+    print("\n1. Print total stock price\n2. Print item stock\n3. Print item price\n4. Quit")
+    try:
+        choice = input("\nEnter what you would like to do (1-4): ")
 
-# Print total stock price
-print("Total stock price = £", total_stock_price)
+        match choice:
+            case "1":
+                # Total stock price calculation
+                total_stock_price = 0
+
+                for item in stock_list:
+                    item_stock_price = item[1] * item[2]
+                    total_stock_price += item_stock_price
+
+                # Print total stock price
+                print("Total stock price = £", total_stock_price)
+
+            case "2":
+                item_found = False
+                item_name = input("\nEnter the name of the item: ")
+                for item in stock_list:
+                    if item_name.lower() == item[0]:
+                        item_found = True
+                        print(f"\nThe stock of {item[0]} is {item[2]}")
+
+                if item_found == False:
+                    print("Item not found")
+
+            case "3":
+                item_found = False
+                item_name = input("\nEnter the name of the item: ")
+                for item in stock_list:
+                    if item_name.lower() == item[0]:
+                        item_found = True
+                        print(f"\nThe stock of {item[0]} is {item[2]}")
+
+                if item_found == False:
+                    print("Item not found")
+
+            case "4":
+                exit_system = True
+
+            case _:
+                print("\nOption not recognized, try again!")
+    except Exception as e:
+        print(f"\nUnknow exception caught - {e}")
+        exit_system = True
